819131e8f26991244bdd290d204111e6163f26b5
Abhay Singh <61372737+Amrit066@users.noreply.github.com>
2023-02-06 00:24:31 +0530

    Built responsive dashboard

---
+++ Dashboard/orders.js
@@ -0,0 +1,45 @@
+const Orders=[
+    {
+        productName: "Asian Shoe",
+        productNumber: 74532,
+        paymentStatus: "Due",
+        shipping: "Pending"
+    },
+    {
+        productName: "Acer Aspire 7 Laptop",
+        productNumber: 67532,
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "Blue Rose Laptop Cover",
+        productNumber: 68532,
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Zebronics Combo Pack",
+        productNumber: 89532,
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "Lenskart Wayfarer Computer Glass",
+        productNumber: 88532,
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    },
+    {
+        productName: "Mi Trimmer",
+        productNumber: 58532,
+        paymentStatus: "Refunded",
+        shipping: "Declined"
+    },
+    {
+        productName: "Milton Hot-Cold Thermos",
+        productNumber: 98532,
+        paymentStatus: "Paid",
+        shipping: "Delivered"
+    }
+]
+
---
+++ Dashboard/script.js
@@ -0,0 +1,40 @@
+
+// SELECTING HTML TAG
+
+const sideMenu=document.querySelector("aside")
+const menuBtn=document.querySelector("#menu-btn")
+const closeBtn=document.querySelector("#close-btn")
+const themeToggler= document.querySelector(".theme-toggler")
+
+// ADDING EVENT LISTENER AND THEIR BEHAVIOUR
+
+menuBtn.addEventListener("click",() =>{
+    sideMenu.style.display="block";
+})
+
+closeBtn.addEventListener("click",() =>{
+    sideMenu.style.display="none";
+})
+
+themeToggler.addEventListener("click",() =>{
+    document.body.classList.toggle("dark-theme-variables");
+        
+    themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+    themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
+
+// ADDING INFO TO TABLE REGARDING ITEMS ORDERED
+Orders.forEach(order => {
+    const tr=document.createElement("tr");
+    const trContent= `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentStatus}</td>
+        <td class="${order.shipping==='Declined'?'danger':order.shipping==='Pending'?'warning':order.shipping==='Delivered'?'success':'primary'}">${order.shipping}</td>
+        <td class="primary">Details</td>
+    `;
+
+    tr.innerHTML=trContent;
+    document.querySelector("table tbody").appendChild(tr);
+})
+
